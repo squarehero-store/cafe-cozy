367324e1af69060d930c4c7c408b71c9a4efeebd
houghtoncreative <info@houghtoncreative.co.nz>
2024-07-17 23:16:26 +1200

    add logs to try solve collection folder backend issue

--- cafe-cozy.js
+++ cafe-cozy.js
@@ -8,76 +8,54 @@
    when a portfolio collection is located inside that folder. */
 
    window.addEventListener('load', function () {
-    setTimeout(function () {
-      function createFolderItems(container, items, isMobile = false) {
-        items.forEach(item => {
-          const folderItemDiv = document.createElement('div');
-          if (isMobile) {
-            folderItemDiv.classList.add('container', 'header-menu-nav-item', 'header-menu-nav-item--external');
-          } else {
-            folderItemDiv.classList.add('header-nav-folder-item', 'header-nav-folder-item--external');
-          }
+    console.log("Window loaded");
   
-          const linkElement = document.createElement('a');
-          linkElement.href = item.fullUrl;
-          linkElement.textContent = item.title;
+    function createFolderItems(container, items, isMobile = false) {
+      console.log("Creating folder items", { container, items, isMobile });
+      items.forEach(item => {
+        const folderItemDiv = document.createElement('div');
+        if (isMobile) {
+          folderItemDiv.classList.add('container', 'header-menu-nav-item', 'header-menu-nav-item--external');
+        } else {
+          folderItemDiv.classList.add('header-nav-folder-item', 'header-nav-folder-item--external');
+        }
   
-          if (isMobile) {
-            linkElement.setAttribute('tabindex', '0');
-          }
+        const linkElement = document.createElement('a');
+        linkElement.href = item.fullUrl;
+        linkElement.textContent = item.title;
   
-          folderItemDiv.appendChild(linkElement);
-          container.appendChild(folderItemDiv);
-        });
-      }
+        if (isMobile) {
+          linkElement.setAttribute('tabindex', '0');
+        }
   
-      function processDesktopDropdownFolders(collections) {
-        const dropdownFolders = document.querySelectorAll('.header-nav-folder-content');
-        dropdownFolders.forEach(folder => {
-          const folderItems = folder.querySelectorAll('.header-nav-folder-item');
+        folderItemDiv.appendChild(linkElement);
+        container.appendChild(folderItemDiv);
+      });
+    }
   
-          folderItems.forEach(folderItem => {
-            const linkElement = folderItem.querySelector('a');
-            if (linkElement) {
-              const collectionPath = linkElement.getAttribute('href');
-              const collectionName = collectionPath.split('/').pop();
+    function processDesktopDropdownFolders(collections) {
+      console.log("Processing desktop dropdown folders", collections);
+      const dropdownFolders = document.querySelectorAll('.header-nav-folder-content');
+      console.log("Found desktop dropdown folders", dropdownFolders);
   
-              if (collections.includes(collectionName)) {
-                const cacheBuster = `cb=${new Date().getTime()}`;
-                fetch(`${collectionPath}?format=json&${cacheBuster}`)
-                  .then(response => {
-                    if (!response.ok) {
-                      throw new Error('Not a collection');
-                    }
-                    return response.json();
-                  })
-                  .then(data => {
-                    const items = data.items;
-                    if (items && items.length > 0) {
-                      folder.innerHTML = ''; // Clear existing content
-                      createFolderItems(folder, items);
-                    }
-                  })
-                  .catch(error => {
-                    console.log(`${collectionPath} is not a collection. Error: ${error}`);
-                  });
-              }
-            }
-          });
-        });
-      }
+      dropdownFolders.forEach(folder => {
+        const folderItems = folder.querySelectorAll('.header-nav-folder-item');
+        console.log("Found folder items in desktop dropdown", folderItems);
   
-      function processMobileDropdownFolders(collections) {
-        const navList = document.querySelector('.header-menu-nav-list');
-        if (navList) {
-          collections.forEach(collection => {
-            const navItems = navList.querySelectorAll(`a[href="/${collection}"]`);
-            navItems.forEach(navItem => {
-              const parentItem = navItem.closest('.header-menu-nav-item');
-              const collectionPath = navItem.getAttribute('href');
+        folderItems.forEach(folderItem => {
+          const linkElement = folderItem.querySelector('a');
+          if (linkElement) {
+            const collectionPath = linkElement.getAttribute('href');
+            const collectionName = collectionPath.split('/').pop();
+  
+            console.log("Checking collection path", collectionPath);
+            if (collections.includes(collectionName)) {
               const cacheBuster = `cb=${new Date().getTime()}`;
+              console.log("Fetching collection data", collectionPath);
+  
               fetch(`${collectionPath}?format=json&${cacheBuster}`)
                 .then(response => {
+                  console.log("Fetch response for desktop dropdown", response);
                   if (!response.ok) {
                     throw new Error('Not a collection');
                   }
@@ -85,41 +63,85 @@
                 })
                 .then(data => {
                   const items = data.items;
+                  console.log("Fetched items for desktop dropdown", items);
                   if (items && items.length > 0) {
-                    const folderContent = parentItem.closest('.header-menu-nav-folder-content');
-                    if (folderContent) {
-                      const controls = folderContent.querySelector('.header-menu-controls');
-                      folderContent.innerHTML = ''; // Clear existing content
-                      if (controls) {
-                        folderContent.appendChild(controls); // Re-add the controls
-                      }
-                      createFolderItems(folderContent, items, true);
-                    }
+                    folder.innerHTML = ''; // Clear existing content
+                    createFolderItems(folder, items);
                   }
                 })
                 .catch(error => {
-                  console.log(`Mobile ${collectionPath} is not a collection. Error: ${error}`);
+                  console.log(`${collectionPath} is not a collection. Error: ${error}`);
                 });
-            });
-          });
-        }
-      }
+            }
+          }
+        });
+      });
+    }
   
-      const metaTag = document.querySelector('meta[squarehero-plugin="collection-folder"]');
-      if (metaTag) {
-        const enabled = metaTag.getAttribute('enabled');
-        if (enabled === "true") {
-          const collections = metaTag.getAttribute('target').split(',').map(item => item.trim());
-          processDesktopDropdownFolders(collections);
-          processMobileDropdownFolders(collections);
-        }
+    function processMobileDropdownFolders(collections) {
+      console.log("Processing mobile dropdown folders", collections);
+      const navList = document.querySelector('.header-menu-nav-list');
+      console.log("Found mobile nav list", navList);
+  
+      if (navList) {
+        collections.forEach(collection => {
+          const navItems = navList.querySelectorAll(`a[href="/${collection}"]`);
+          console.log("Found nav items for collection", collection, navItems);
+  
+          navItems.forEach(navItem => {
+            const parentItem = navItem.closest('.header-menu-nav-item');
+            const collectionPath = navItem.getAttribute('href');
+            const cacheBuster = `cb=${new Date().getTime()}`;
+  
+            console.log("Fetching collection data for mobile dropdown", collectionPath);
+            fetch(`${collectionPath}?format=json&${cacheBuster}`)
+              .then(response => {
+                console.log("Fetch response for mobile dropdown", response);
+                if (!response.ok) {
+                  throw new Error('Not a collection');
+                }
+                return response.json();
+              })
+              .then(data => {
+                const items = data.items;
+                console.log("Fetched items for mobile dropdown", items);
+                if (items && items.length > 0) {
+                  const folderContent = parentItem.closest('.header-menu-nav-folder-content');
+                  if (folderContent) {
+                    const controls = folderContent.querySelector('.header-menu-controls');
+                    folderContent.innerHTML = ''; // Clear existing content
+                    if (controls) {
+                      folderContent.appendChild(controls); // Re-add the controls
+                    }
+                    createFolderItems(folderContent, items, true);
+                  }
+                }
+              })
+              .catch(error => {
+                console.log(`Mobile ${collectionPath} is not a collection. Error: ${error}`);
+              });
+          });
+        });
       }
-    }, 1000); // 1-second delay to ensure all dynamic elements are fully rendered
+    }
+  
+    const metaTag = document.querySelector('meta[squarehero-plugin="collection-folder"]');
+    console.log("Found meta tag", metaTag);
+  
+    if (metaTag) {
+      const enabled = metaTag.getAttribute('enabled');
+      console.log("Meta tag enabled attribute", enabled);
+  
+      if (enabled === "true") {
+        const collections = metaTag.getAttribute('target').split(',').map(item => item.trim());
+        console.log("Collections to process", collections);
+  
+        processDesktopDropdownFolders(collections);
+        processMobileDropdownFolders(collections);
+      }
+    }
   });
   
-
-
-
 // ==================================
 //   Common SquareHero Template Files   
 // ==================================
